6bc6b175d8d55d0cd2f83a56cbcb980788eba85d
Luciano Cardozo Casariego <lucianocardozocasariego@gmail.com>
2024-10-14 23:44:54 -0300

    fix

--- Views/pre_game/formation_selector.gd
+++ Views/pre_game/formation_selector.gd
@@ -14,9 +14,11 @@ func init_formation_selection(playerID: int):
 func generate_options():
 	var formations_scenes = Formations.formations_bank 
 	var op_position = 270 if(player == 2) else 50
+
 	for formation_scene in formations_scenes:
 		var formation_instance = formation_scene.instantiate() as Formation_template
 		options.add_child(formation_instance)
+		
 		if(player == 2):
 			formation_instance.set_formation_to_player_2()
 
@@ -24,11 +26,11 @@ func generate_options():
 		op_position += 270
 		formation_instance.position.y += -100
 		
-		formation_instance.set_reference_visible(true)
-		formation_instance.fill_marcks_width_points()
 		formation_instance.scale = Vector2(0.2, 0.2)
 		formation_instance.scene = formation_scene
 		formation_instance.get_on_click_signal().connect(on_select_formation)
+		formation_instance.set_reference_visible(true)
+		formation_instance.fill_marcks_width_points()
 
 func select_current_formation():
 	var current
